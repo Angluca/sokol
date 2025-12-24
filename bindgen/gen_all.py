c0fde8c7670968cc28f53ca573d11a73cd63422f
Pixeller <156404+Angluca@users.noreply.github.com>
2025-12-24 21:07:02 +0800

    update

--- bindgen/gen_all.py
+++ bindgen/gen_all.py
@@ -1,4 +1,4 @@
-import os, argparse, gen_nim, gen_zig, gen_odin, gen_rust, gen_d, gen_jai, gen_c3, shutil
+import os, argparse, gen_nim, gen_zig, gen_odin, gen_rust, gen_d, gen_jai, gen_c3, gen_nature, shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--zig-tiger-style", action="store_true", help="Enable zig tiger style mode.")
@@ -16,66 +16,72 @@ tasks = [
     [ '../util/sokol_shape.h',     'sshape_',   ['sg_'] ],
 ]
 
-# Jai
-gen_jai.prepare()
+# # Jai
+# gen_jai.prepare()
+# for task in tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_jai.gen(c_header_path, main_prefix, dep_prefixes)
+
+# # Odin
+# gen_odin.prepare()
+# for task in tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_odin.gen(c_header_path, main_prefix, dep_prefixes)
+
+# # Nim
+# gen_nim.prepare()
+# for task in tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_nim.gen(c_header_path, main_prefix, dep_prefixes)
+
+# # Zig
+# zig_tasks = [
+    # *tasks,
+    # [ '../sokol_fetch.h', 'sfetch_', [] ],
+    # [ '../util/sokol_imgui.h', 'simgui_',   ['sg_', 'sapp_'] ],
+    # [ '../util/sokol_gfx_imgui.h', 'sgimgui_', [] ],
+# ]
+# gen_zig.prepare()
+# for task in zig_tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_zig.gen(c_header_path, main_prefix, dep_prefixes, {"tiger-style": args.zig_tiger_style})
+
+# # D
+# d_tasks = [
+    # *tasks,
+    # [ '../sokol_args.h',  'sargs_',  [] ],
+    # [ '../sokol_fetch.h', 'sfetch_', [] ],
+    # [ '../util/sokol_memtrack.h', 'smemtrack_', [] ],
+    # [ '../util/sokol_imgui.h', 'simgui_',   ['sg_', 'sapp_'] ],
+    # [ '../util/sokol_gfx_imgui.h', 'sgimgui_',   ['sg_', 'sapp_'] ],
+# ]
+# # check if nuklear.h is available and copy it
+# if os.path.exists('../tests/ext/nuklear.h'):
+    # d_tasks.append([ '../util/sokol_nuklear.h', 'snk_',   ['sg_', 'sapp_'] ])
+    # if os.path.exists('sokol-d'):
+        # shutil.copy('../tests/ext/nuklear.h', 'sokol-d/src/sokol/c/nuklear.h')
+# gen_d.prepare()
+# for task in d_tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_d.gen(c_header_path, main_prefix, dep_prefixes)
+# # drop nuklear.h if copied (after generated D files)
+# if os.path.exists('sokol-d/src/sokol/c/nuklear.h'):
+    # os.remove('sokol-d/src/sokol/c/nuklear.h')
+
+# # Rust
+# gen_rust.prepare()
+# for task in tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_rust.gen(c_header_path, main_prefix, dep_prefixes)
+
+# # C3
+# gen_c3.prepare()
+# for task in tasks:
+    # [c_header_path, main_prefix, dep_prefixes] = task
+    # gen_c3.gen(c_header_path, main_prefix, dep_prefixes)
+
+# nature
+gen_nature.prepare()
 for task in tasks:
     [c_header_path, main_prefix, dep_prefixes] = task
-    gen_jai.gen(c_header_path, main_prefix, dep_prefixes)
-
-# Odin
-gen_odin.prepare()
-for task in tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_odin.gen(c_header_path, main_prefix, dep_prefixes)
-
-# Nim
-gen_nim.prepare()
-for task in tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_nim.gen(c_header_path, main_prefix, dep_prefixes)
-
-# Zig
-zig_tasks = [
-    *tasks,
-    [ '../sokol_fetch.h', 'sfetch_', [] ],
-    [ '../util/sokol_imgui.h', 'simgui_',   ['sg_', 'sapp_'] ],
-    [ '../util/sokol_gfx_imgui.h', 'sgimgui_', [] ],
-]
-gen_zig.prepare()
-for task in zig_tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_zig.gen(c_header_path, main_prefix, dep_prefixes, {"tiger-style": args.zig_tiger_style})
-
-# D
-d_tasks = [
-    *tasks,
-    [ '../sokol_args.h',  'sargs_',  [] ],
-    [ '../sokol_fetch.h', 'sfetch_', [] ],
-    [ '../util/sokol_memtrack.h', 'smemtrack_', [] ],
-    [ '../util/sokol_imgui.h', 'simgui_',   ['sg_', 'sapp_'] ],
-    [ '../util/sokol_gfx_imgui.h', 'sgimgui_',   ['sg_', 'sapp_'] ],
-]
-# check if nuklear.h is available and copy it
-if os.path.exists('../tests/ext/nuklear.h'):
-    d_tasks.append([ '../util/sokol_nuklear.h', 'snk_',   ['sg_', 'sapp_'] ])
-    if os.path.exists('sokol-d'):
-        shutil.copy('../tests/ext/nuklear.h', 'sokol-d/src/sokol/c/nuklear.h')
-gen_d.prepare()
-for task in d_tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_d.gen(c_header_path, main_prefix, dep_prefixes)
-# drop nuklear.h if copied (after generated D files)
-if os.path.exists('sokol-d/src/sokol/c/nuklear.h'):
-    os.remove('sokol-d/src/sokol/c/nuklear.h')
-
-# Rust
-gen_rust.prepare()
-for task in tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_rust.gen(c_header_path, main_prefix, dep_prefixes)
-
-# C3
-gen_c3.prepare()
-for task in tasks:
-    [c_header_path, main_prefix, dep_prefixes] = task
-    gen_c3.gen(c_header_path, main_prefix, dep_prefixes)
+    gen_nature.gen(c_header_path, main_prefix, dep_prefixes)
